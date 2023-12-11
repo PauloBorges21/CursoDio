3bac947f63f68d5ee5f680daff0d1ea6c8da3e7a
paulo.borges <paulo.borges@orsoftware.com.br>
2023-12-11 15:41:44 -0300

    Método de Add,remover e listar finalizados

--- prop-metod-construtor/Models/Curso.cs
+++ prop-metod-construtor/Models/Curso.cs
@@ -12,9 +12,31 @@ namespace prop_metod_construtor.Models
         public List<Pessoa> Alunos { get; set; }
 
 
-         void AdicionarAluno(Pessoa aluno)
+        public void AdicionarAluno(Pessoa aluno)
         {
             Alunos.Add(aluno);
+        }
+
+        public void RemoverAluno(Pessoa aluno)
+        {
+            Alunos.Remove(aluno);
+        }
+
+        public int QuantidadeAlunoMatriculados()
+        {
+            int quatidadeAlunos = Alunos.Count;
+            return quatidadeAlunos;
+        }
+
+
+        public void ListarAlunos()
+        {   
+             Console.WriteLine($"Alunos do curso: {Nome}") ;
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto) ;
+            }
+            
         }
 
     }
--- prop-metod-construtor/Program.cs
+++ prop-metod-construtor/Program.cs
@@ -5,6 +5,25 @@ Pessoa p1 = new Pessoa();
 
 p1.Nome = "Paulo";
 p1.Sobrenome = "Borges";
-p1.Idade = 10;
 
-p1.Apresentar();
+
+// p1.Idade = 10;
+
+// p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+Curso c = new Curso();
+p2.Nome = "Pedro";
+p2.Sobrenome = "Souza";
+
+c.Nome = "Matematica";
+
+c.Alunos = new List<Pessoa>();
+
+c.AdicionarAluno(p1);
+c.AdicionarAluno(p2);
+
+c.ListarAlunos();
+
+c.RemoverAluno(p1);
+c.ListarAlunos();
